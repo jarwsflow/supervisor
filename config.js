5a6da50d3717229ac17a49c81b1223e8631e0159
jarwsflow <70908775+jarwsflow@users.noreply.github.com>
2023-07-02 02:24:19 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+﻿const config = {
+  "botStatus": ["Selam"],
+  "prefix": ["."],
+  "token": "MTEyNDgwMDM3NDIwNTg0OTY1MA.GXo_VK.43WWgFe5-iQWotTXZnWko9zJImlYBzDj_pfN_A",
+  "mongoose": "mongodb+srv:s=true&w=majority",
+  "botOwners": ["720307947569283106"],
+  "guildID": "1124068723469328486",
+  
+  "emojis": {
+    "yes_name": "yes_ramal",
+    "no_name": "no_ramal",
+    "mute_name": "ramal_mute"
+  }
+  }
+  
+  module.exports = config;
+  
